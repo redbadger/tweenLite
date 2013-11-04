1ec6b58aab3a8189802a40949a7d2b43c2051892
Dario Villanueva <dario.villanueva@gmail.com>
2013-11-04 15:38:19 +0000

    test

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // just require these to initialise tweenlite in your window object
-module.exports = function() {
-  require('./libs/CSSplugin');
-  require('./libs/EasePack');
-  require('./libs/ScrollToPlugin');
-  require('./libs/TweenLite');
-}
+require('./libs/CSSplugin');
+require('./libs/EasePack');
+require('./libs/ScrollToPlugin');
+require('./libs/TweenLite');
 
+
+module.exports = window.TweenLite;
